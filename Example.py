05bf796dbef194d9068ceac4fd2634af990b3092
Arnab Bhattacharyya <arnabbhattacharyya6294@gmail.com>
2023-03-13 21:34:16 +0530

    Creating Average Module

--- Example.py
+++ Example.py
@@ -1,6 +1,11 @@
 import AverageModule
 
+# DefaultStyle
 numbers = int(input("\nEnter number of elements: "))
-# print("\nEnter numbers: ") 
 avg = AverageModule.Average(numbers)
 print(f"\nThe average of {numbers} numbers is: ", avg.average())
+
+# DefaultStyle False
+print("\nEnter numbers: ") 
+avg = AverageModule.Average(numbers)
+print(f"\nThe average of {numbers} numbers is: ", avg.average(defaultStyle=False))
